93f113a5185830c34200e49d14ac204f7b9dc25d
horaciocoronel <arielc@gmail.com>
2017-10-10 15:33:51 -0400

    Add functionality to form to push data into DB

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,6 +9,18 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
   def create
-    render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      # If the picture gets saved, generate a request to '/pictures' (the index)
+      redirect_to "/pictures"
+    else
+      # Otherwise render new.html.erb
+      render :new
+    end
   end
 end
