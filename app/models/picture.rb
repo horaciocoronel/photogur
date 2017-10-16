8bd170bf05e06aaa9cb87f63095d0b8a0cf20d2f
horaciocoronel <arielc@gmail.com>
2017-10-16 16:05:19 -0400

    Add regular expression for http

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,9 @@
 class Picture < ApplicationRecord
 
   validates :artist, :title, :url, presence: true
+  validates :title, length: { in: 3..20 }
+  validates :url, uniqueness: true
+  validates_format_of :url, :with => URI::regexp(%w(http https))
 
   def self.one_month_ago
     Picture.where(("created_at < ?"), 1.month.ago).limit(5)
