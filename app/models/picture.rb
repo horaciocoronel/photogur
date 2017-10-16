f6846ee408871bf264aaea22dd2a894b20f92e42
horaciocoronel <arielc@gmail.com>
2017-10-16 15:05:07 -0400

    Add 'pictures_created_in_year' method

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,10 @@
 class Picture < ApplicationRecord
 
   def self.one_month_ago
-    Picture.where(("created_at > ?"), 1.month.ago).limit(5)
+    Picture.where(("created_at < ?"), 1.month.ago).limit(5)
   end
 
+  def self.pictures_created_in_year(year)
+    Picture.where("cast(strftime('%Y', created_at) as int) = ?", year)
+  end
 end
