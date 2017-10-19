8ca9826496ed1fae4a4318ff90accb3438428141
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-19 14:41:08 -0400

    resource  :users,  only: %i(create new)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resource  :users,  only: %i(create new)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pictures#index'
