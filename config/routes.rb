f6196be203cdc63f66cef14c9b8f2356f7fcbc4e
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-19 14:50:55 -0400

    deletes two sessions files and adds resources to the Sessions Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resource  :users,  only: %i(create new)
+  resource  :sessions, only: %i(create new destroy)
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
