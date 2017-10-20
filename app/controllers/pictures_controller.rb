b0c2e4e115b0db679f5ba227af5bf5cbdae7e7d3
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-20 16:04:00 -0400

    Revert "set up a before_action that uses this ensure_logged_in method in our PicturesController"
    This reverts commit c29795fbb684f9b513f590225248b29c6752fe36.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,4 @@
 class PicturesController < ApplicationController
-
-  before_action :ensure_logged_in, except: [:show, :index]
-
-
   def index
     @pictures = Picture.all
     @one_month_ago = Picture.one_month_ago
